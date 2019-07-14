c89471f68f9ca6b36ceeb139fd1262e0e120c721
Desislav Arashev <darashev@cst-bg.net>
2019-07-15 00:30:01 +0300

    Add timer accuracy

--- public/js/Layers.js
+++ public/js/Layers.js
@@ -22,3 +22,9 @@ export function createBackgroundLayer(backgrounds, sprites) {
 
     }
 }
+
+export function createSpritesLayer(entity) {
+    return function drawSpriteLayer(context) {
+        entity.draw(context)
+    }
+}
